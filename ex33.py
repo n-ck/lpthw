9f55bcd8cfd223b5145e5a8830ec89cc2e463f22
Nick Kuizenga <nickkuis@hotmail.com>
2018-09-05 21:39:40 -0400

    Fixed the for loop based function in exercise 33

--- ex33.py
+++ ex33.py
@@ -24,15 +24,23 @@ my_numbers(0, 10, 2)
 
 def for_numbers(i, top, incr):
 
-	numbers = []
+	# Create a list that just contains i
+	numbers = [i]
 
+	# for variable i in the numbers list:
 	for i in numbers:
-		if i < 12: 
+		# if i is smaller than top [given in the function]
+		if i < top: 
+			# print:
 			print "At the top i is %d" % i
+			
+			# then increment i with the incr variable
+			i = i + incr
+			# and append the numbers list with the new i value
 			numbers.append(i)
 
-			i = i + incr
+			# print the list and the value of i
 			print "Numbers now: ", numbers
 			print "At the bottom i is %d" % i
 
-for_numbers(0, 10, 2)
+for_numbers(0, 10, 1)
