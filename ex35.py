7a862d388c46b4a33e98d411f4c25e427bdb5e36
Nick Kuizenga <nickkuis@hotmail.com>
2018-09-07 19:47:18 -0400

    Updated the exercise

--- ex35.py
+++ ex35.py
@@ -5,10 +5,12 @@ def gold_room():
 	print "This room is full of gold. How much do you take?"
 
 	next = raw_input("> ")
-	if "0" in next or "1" in next:
-		how_much = int(next)
-	else:
-		dead("Man, learn to type a number.")
+	how_much = int(next)
+	
+	# if "0" in next or "1" in next:
+	# 	how_much = int(next)
+	# else:
+	# 	dead("Man, learn to type a number.")
 
 	if how_much < 50:
 		print "Nice, you're not greedy, you win!"
@@ -20,8 +22,8 @@ def bear_room():
 	'''This is the first level of the game if you choose the left door'''
 	print "There is a bear here."
 	print "The bear has a bunch of honey."
-	print "The fat bear is in fron of another door"
-	print "How are you going to move the bear?"
+	print "The fat bear is in front of another door"
+	print "How are you going to move the bear? [taunt bear or take honey]"
 	bear_moved = False
 
 	while True:
@@ -43,7 +45,7 @@ def cthulhu_room():
 	'''First level of the game if you choose the right door'''
 	print "Here you see the great evil Cthulhu."
 	print "He, it, whatever stares at you and you go insane."
-	print "Do you flee for your life or eat your head?"
+	print "Do you flee for your life or eat your head? [flee or head]"
 
 	next = raw_input("> ")
 
@@ -64,7 +66,7 @@ def start():
 	'''This function starts the game off'''
 	print "you are in a dark room"
 	print "There is a door to your right and left"
-	print "Which one do you take?"
+	print "Which one do you take? [left or right]"
 
 	next = raw_input("> ")
 
