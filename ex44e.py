a150057a1c1a0ae410a1e73f5b9db910395d29f2
Nick Kuizenga <nickkuis@hotmail.com>
2018-10-03 22:04:16 -0400

    added more examples

--- ex44e.py
+++ ex44e.py
@@ -31,3 +31,50 @@ son = Child()
 son.implicit()
 son.override()
 son.altered()
+
+
+## Class Inheritance
+
+class Parent(object):
+
+	def lastname(self, lastname):
+		self.lastname = lastname
+		print self.lastname
+
+class Child(Parent):
+
+	def firstname(self, firstname):
+		self.firstname = firstname
+		print self.firstname
+
+
+name = Child()
+
+name.firstname('First')
+name.lastname('Last')
+
+## Class Composition
+
+class Brand(object):
+
+	def brandname(self, make):
+		self.make = make
+		print self.make
+
+class Car(object):
+
+	def __init__(self):
+		self.brand = Brand()
+
+	def carbrand(self):
+		self.brand.brandname()
+		
+	def cartype(self, carmodel):
+		self.carmodel = carmodel
+		print self.carmodel
+
+name = Car()
+
+name.carbrand('BMW')
+name.cartype('sedan')
+
