36dd3fd9cf93a4e0a3e0522c82be15acfa751690
Nick Kuizenga <nickkuis@hotmail.com>
2018-10-05 19:13:40 -0400

    started game of ex43

---
+++ ex45.py
@@ -0,0 +1,28 @@
+## Ex45 Game
+
+# import multiple files
+# use one class per room
+# make one class that knows about the rooms and runs them
+# spend a week creating this game
+# use classes, functions, dicts, lists and loops
+# purpose of the lesson is to teach how to structure classes
+# that need other classes inside other files
+
+# Game description:
+# Pick one out of 3 doors for every room
+# Based on the door picked you go the the next room
+# 2 doors lead to the next room, in one door you die
+# 
+
+
+class Rooms():
+	pass
+
+class Dead():
+	pass
+
+class StartGame():
+	pass
+
+class Doors():
+	pass
