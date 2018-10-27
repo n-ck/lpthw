525fb6c9f64e450da9e66c4673a1b5c589957183
Nick Kuizenga <nickkuis@hotmail.com>
2018-10-27 12:40:46 -0400

    script works but with wrong tuple order

--- projects/ex48/ex48/lexicon.py
+++ projects/ex48/ex48/lexicon.py
@@ -19,29 +19,55 @@ def scan(sentence):
  	for word in splitsentence:
  		if word in directions:
  			param = "direction"
- 			directiontuple = (param, word)
- 		 	newtuple.append(directiontuple)
- 		 	print newtuple
 
 		elif word in verbs:
 			param = "verb"
-			directiontuple = (param, word)
-			newtuple.append(directiontuple)
-			return newtuple
 
 		elif word in stopwords:
 			param = "stop"
-			directiontuple = (param, word)
-			return newtuple.append(directiontuple)
 
 		elif word in nouns:
 			param = "noun"
-			directiontuple = (param, word)
-			return newtuple.append(directiontuple)
+			
+
+	while splitsentence != []:
+		for word in splitsentence:
+			newword = splitsentence.pop()
+			tuplecombo = (param, newword)
+			newtuple.append(tuplecombo)
+
+	return newtuple
  	
 # # needs an if statement, if single word don't create a list, just print the tuple
-# scan('north')
+# print scan("north")
+
+# print scan("north south east")
 
 # # this is working
 # scan('south north east')
 
+
+## Working for loop but only returns one tuple:
+
+ 	# for word in splitsentence:
+ 	# 	if word in directions:
+ 	# 		param = "direction"
+ 	# 		directiontuple = (param, word)
+ 	# 	 	newtuple.append(directiontuple)
+ 	# 	 	return newtuple
+
+		# elif word in verbs:
+		# 	param = "verb"
+		# 	directiontuple = (param, word)
+		# 	newtuple.append(directiontuple)
+		# 	return newtuple
+
+		# elif word in stopwords:
+		# 	param = "stop"
+		# 	directiontuple = (param, word)
+		# 	return newtuple.append(directiontuple)
+
+		# elif word in nouns:
+		# 	param = "noun"
+		# 	directiontuple = (param, word)
+		# 	return newtuple.append(directiontuple)
