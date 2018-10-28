972c4492b245aa744cbdf3642e5c096fdd02dfdc
Nick Kuizenga <nickkuis@hotmail.com>
2018-10-27 20:51:10 -0400

    passes 4 out of 6, rewriting for number entries

--- projects/ex48/ex48/lexicon.py
+++ projects/ex48/ex48/lexicon.py
@@ -1,74 +1,113 @@
 
-
 def scan(sentence):
 	splitsentence = sentence.split()
 
 	directions = ["north", "south", "east", "west",
 				  "down", "up", "left", "right", "back"]
-
 	verbs = ["go", "stop", "kill", "eat"]
-
 	stopwords = ["the", "in", "of", "from", "at", "it"]
-
 	nouns = ["door", "bear", "princess", "cabinet"]
-
-	numbers = range(0,10)
 
 	newtuple = []
 
- 	for word in splitsentence:
- 		if word in directions:
- 			param = "direction"
+	for word in splitsentence:
 
-		elif word in verbs:
-			param = "verb"
+		try:
+			int(word)
+			scannumber(sentence)
+		except ValueError:
+			scanword(sentence)
 
-		elif word in stopwords:
-			param = "stop"
+	## working code but numbers aren't working:
+	
+	# while splitsentence != []:
+	# 	for word in splitsentence:
+	# 		newword = splitsentence.pop()
 
-		elif word in nouns:
-			param = "noun"
+	#  		if word in directions:
+	#  			param = "direction"
 
+	# 		elif word in verbs:
+	# 			param = "verb"
+
+	# 		elif word in stopwords:
+	# 			param = "stop"
+
+	# 		elif word in nouns:
+	# 			param = "noun"
+
+	# 		elif int(word):
+	# 			newword = int(word)
+	# 			param = "number"
+
+	# 		else:
+	# 			param = "error"
+
+	# 		tuplecombo = (param, newword)
+	# 		newtuple.append(tuplecombo)
+
+	# newtuple.reverse()
+	# return newtuple
+
+def scanword(sentence):
+
+	directions = ["north", "south", "east", "west",
+				  "down", "up", "left", "right", "back"]
+	verbs = ["go", "stop", "kill", "eat"]
+	stopwords = ["the", "in", "of", "from", "at", "it"]
+	nouns = ["door", "bear", "princess", "cabinet"]
+
+	newtuple = []
 
 	while splitsentence != []:
 		for word in splitsentence:
 			newword = splitsentence.pop()
+
+	 		if word in directions:
+	 			param = "direction"
+
+			elif word in verbs:
+				param = "verb"
+
+			elif word in stopwords:
+				param = "stop"
+
+			elif word in nouns:
+				param = "noun"
+
+			else:
+				param = "error"
+
 			tuplecombo = (param, newword)
 			newtuple.append(tuplecombo)
 
 	newtuple.reverse()
 	return newtuple
+
+
+def scannumber(sentence):
+
+	newtuple = []
+
+	while splitsentence != []:
+		for word in splitsentence:
+			newword = splitsentence.pop()
+	 		param = "number"
+			tuplecombo = (param, newword)
+			newtuple.append(tuplecombo)
+
+	newtuple.reverse()
+	return newtuple[]
  	
 # # needs an if statement, if single word don't create a list, just print the tuple
 # print scan("north")
 
 # print scan("north south east")
 
-# # this is working
-# scan('south north east')
+print scan("1234")
 
+print scan("3 91234")
 
-## Working for loop but only returns one tuple:
+# print scan("ASDFADFASDF")
 
- 	# for word in splitsentence:
- 	# 	if word in directions:
- 	# 		param = "direction"
- 	# 		directiontuple = (param, word)
- 	# 	 	newtuple.append(directiontuple)
- 	# 	 	return newtuple
-
-		# elif word in verbs:
-		# 	param = "verb"
-		# 	directiontuple = (param, word)
-		# 	newtuple.append(directiontuple)
-		# 	return newtuple
-
-		# elif word in stopwords:
-		# 	param = "stop"
-		# 	directiontuple = (param, word)
-		# 	return newtuple.append(directiontuple)
-
-		# elif word in nouns:
-		# 	param = "noun"
-		# 	directiontuple = (param, word)
-		# 	return newtuple.append(directiontuple)
+# print scan("bear IAS princess")
