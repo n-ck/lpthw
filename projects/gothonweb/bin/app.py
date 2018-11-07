e4f0896fbe27c2d99822dc19031a65322b7f33a2
Nick <nickkuizenga@gmail.com>
2018-11-07 18:06:42 -0500

    added second page to website

--- projects/gothonweb/bin/app.py
+++ projects/gothonweb/bin/app.py
@@ -1,7 +1,8 @@
 import web
 
 urls = (
-	'/', 'Index'
+	'/', 'Index',
+	'/test', 'Test',
 )
 
 app = web.application(urls, globals())
@@ -10,8 +11,14 @@ render = web.template.render('templates/')
 
 class Index(object):
 	def GET(self):
-		greeting = "Hello World"
-		return render.index(greeting = greeting)
+		# enter greeting in url as /?name=Nick
+		greeting = web.input(name=None)
+		return render.index(greeting = greeting.name)
+
+class Test(object):
+	def GET(self):
+		message = "Test 123"
+		return render.test(message = message)
 
 if __name__=="__main__":
 	app.run()
