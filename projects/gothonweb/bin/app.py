d22685b1fcf90721cd55e52b49ea7123fea042b8
Nick Kuizenga <nickkuis@hotmail.com>
2018-11-10 21:47:17 -0500

    started ex51

--- projects/gothonweb/bin/app.py
+++ projects/gothonweb/bin/app.py
@@ -1,8 +1,7 @@
 import web
 
 urls = (
-	'/', 'Index',
-	'/test', 'Test',
+	'/hello', 'Index',
 )
 
 app = web.application(urls, globals())
@@ -12,13 +11,25 @@ render = web.template.render('templates/', base='base')
 class Index(object):
 	def GET(self):
 		# enter greeting in url as /?name=Nick
-		greeting = web.input(name=None)
-		return render.index(greeting = greeting.name)
+		# greeting = web.input(name=None)
+		# return render.index(greeting = greeting.name)
 
-class Test(object):
-	def GET(self):
-		message = "Test 123"
-		return render.test(message = message)
+		form = web.input(name="Nobody", greet=None)
+
+		if form.greet:
+			greeting = "%s, %s" % (form.greet, form.name)
+			return render.index(greeting = greeting)
+		else:
+			return "ERROR: greet is required."
+
+		# greeting = "%s, %s" % (form.greet, form.name)
+
+		# return render.index(greeting = greeting)
+
+# class Test(object):
+# 	def GET(self):
+# 		message = "Test 123"
+# 		return render.test(message = message)
 
 if __name__=="__main__":
 	app.run()
